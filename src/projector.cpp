e2d260d6747fe26f0d189a7486b6264600da2cac
Natalie Wolfe <natalie@lifewanted.com>
2021-04-27 10:01:21 -0700

    Fix unprojection z-axis stack issue.

--- src/projector.cpp
+++ src/projector.cpp
@@ -1,18 +1,67 @@
 #include <cmath>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
 #include <vector>
 
 #include "src/cameras.h"
 #include "src/files.h"
 #include "src/tracking.h"
 
+template <typename T>
+cv::Mat cam_extrinsic_matrix(const CameraParameters& params) {
+  // Convert the rvec into a 3x3 matrix.
+  cv::Mat rotation;
+  cv::Rodrigues(params.rotation, rotation);
 
-// Possible fixes:
-// - Recalibrate cameras @ 640x480
-// - Run remapping through calib3d: stereoRectify, undistortRectifyMap, remap, StereoBM
+  // Move camera's position into world space.
+  cv::Mat translation = cv::Mat::zeros(1, 3, CV_64F);
+  translation.at<double>(0, 0) = params.translation.at<double>(0, 0);
+  translation.at<double>(0, 1) = params.translation.at<double>(1, 0);
+  translation.at<double>(0, 2) = params.translation.at<double>(2, 0);
+  translation = translation * rotation;
 
+  // Compose transformation matrix.
+  cv::Mat transformation = cv::Mat::zeros(3, 4, CV_32F);
+  transformation.at<T>(0, 0) = static_cast<T>(rotation.at<double>(0, 0));
+  transformation.at<T>(0, 1) = static_cast<T>(rotation.at<double>(0, 1));
+  transformation.at<T>(0, 2) = static_cast<T>(rotation.at<double>(0, 2));
+  transformation.at<T>(1, 0) = static_cast<T>(rotation.at<double>(1, 0));
+  transformation.at<T>(1, 1) = static_cast<T>(rotation.at<double>(1, 1));
+  transformation.at<T>(1, 2) = static_cast<T>(rotation.at<double>(1, 2));
+  transformation.at<T>(2, 0) = static_cast<T>(rotation.at<double>(2, 0));
+  transformation.at<T>(2, 1) = static_cast<T>(rotation.at<double>(2, 1));
+  transformation.at<T>(2, 2) = static_cast<T>(rotation.at<double>(2, 2));
+  transformation.at<T>(0, 3) = -static_cast<T>(translation.at<double>(0, 0));
+  transformation.at<T>(1, 3) = -static_cast<T>(translation.at<double>(0, 1));
+  transformation.at<T>(2, 3) = -static_cast<T>(translation.at<double>(0, 2));
+
+  return transformation;
+}
+
+cv::Mat convert_mat_to_float(const cv::Mat& source) {
+  cv::Mat matrix = cv::Mat::zeros(source.size(), CV_32F);
+  for (int x = 0; x < source.size().height; ++x) {
+    for (int y = 0; y < source.size().width; ++y) {
+      matrix.at<float>(x, y) = static_cast<float>(source.at<double>(x, y));
+    }
+  }
+  return matrix;
+}
+
+cv::Mat calc_projection(const CameraParameters& params) {
+  cv::Mat matrix = convert_mat_to_float(params.matrix);
+  cv::Mat out_matrix;
+  out_matrix = matrix * cam_extrinsic_matrix<float>(params);
+
+  return out_matrix;
+}
+
+float calc_magnitude(float x, float y, float z) {
+  return std::sqrt((x * x) + (y * y) + (z * z));
+}
 
 /**
  * Computes a unit vector in world-space pointing from the camera through the
@@ -20,27 +69,55 @@
  */
 cv::Mat to_ray(const CameraParameters& params, const Point& point) {
   // TODO: Switch to undistorted matrix.
-  double f_x = params.matrix.at<double>(0, 0);
-  double c_x = params.matrix.at<double>(0, 2);
-  double f_y = params.matrix.at<double>(1, 1);
-  double c_y = params.matrix.at<double>(1, 2);
+  cv::Mat matrix = cv::getOptimalNewCameraMatrix(
+    params.matrix,
+    params.distortion,
+    {640, 480},
+    0.0,
+    {640, 480}
+  );
+  float f_x = static_cast<float>(matrix.at<double>(0, 0));
+  float c_x = static_cast<float>(matrix.at<double>(0, 2));
+  float f_y = static_cast<float>(matrix.at<double>(1, 1));
+  float c_y = static_cast<float>(matrix.at<double>(1, 2));
 
   // Ray from camera origin pointing at pixel on image plane.
-  double p_x = (point.x - c_x) / f_x;
-  double p_y = (point.y - c_y) / f_y;
-  double p_z = 1;
-  double magnitude = std::sqrt((p_x * p_x) + (p_y * p_y) + (p_z * p_z));
+  float p_x = (point.x - c_x) / f_x;
+  float p_y = (point.y - c_y) / f_y;
+  float p_z = 1;
+  float magnitude = calc_magnitude(p_x, p_y, p_z);
   p_x /= magnitude;
   p_y /= magnitude;
   p_z /= magnitude;
-  cv::Vec3d cam_ray{p_x, p_y, p_z};
+
+  cv::Mat cam_ray = cv::Mat::zeros(1, 3, CV_32F);
+  cam_ray.at<float>(0, 0) = p_x;
+  cam_ray.at<float>(0, 1) = p_y;
+  cam_ray.at<float>(0, 2) = p_z;
 
   // Convert the rvec into a 3x3 matrix.
-  cv::Mat transform;
-  cv::Rodrigues(params.rotation, transform);
+  cv::Mat transform = cam_extrinsic_matrix<float>(params);
 
-  cv::Mat origin_ray = transform * cam_ray;
-  return origin_ray;
+  cv::Mat origin_ray = cam_ray * transform;
+  float x = origin_ray.at<float>(0, 0);
+  float y = origin_ray.at<float>(0, 1);
+  float z = origin_ray.at<float>(0, 2);
+  magnitude = calc_magnitude(x, y, z);
+  cam_ray = cv::Mat::zeros(3, 1, CV_32F);
+  cam_ray.at<float>(0, 0) = x / magnitude;
+  cam_ray.at<float>(1, 0) = y / magnitude;
+  cam_ray.at<float>(2, 0) = z / magnitude;
+
+  return cam_ray;
+}
+
+cv::Mat cam_trans_to_world(const CameraParameters& params) {
+  cv::Mat extrinsics = cam_extrinsic_matrix<float>(params);
+  cv::Mat translation = cv::Mat::zeros(3, 1, CV_32F);
+  translation.at<float>(0, 0) = extrinsics.at<float>(0, 3);
+  translation.at<float>(1, 0) = extrinsics.at<float>(1, 3);
+  translation.at<float>(2, 0) = extrinsics.at<float>(2, 3);
+  return translation;
 }
 
 Point3d project_point(
@@ -53,10 +130,18 @@ Point3d project_point(
   // See this answer for the equations followed here and the origin of the
   // variable naming.
   // https://math.stackexchange.com/a/1037202/918090
-  cv::Mat cam_1 = params_1.translation; // a
-  cv::Mat cam_2 = params_2.translation; // c
+  cv::Mat cam_1 = cam_trans_to_world(params_1); // a
+  cv::Mat cam_2 = cam_trans_to_world(params_2); // c
   cv::Mat ray_1 = to_ray(params_1, point_1); // b
   cv::Mat ray_2 = to_ray(params_2, point_2); // d
+
+  // return Point3d{
+  //   .point_id = point_1.point_id,
+  //   .x = ray_1.at<float>(0, 0),
+  //   .y = ray_1.at<float>(1, 0),
+  //   .z = ray_1.at<float>(2, 0),
+  //   .confidence = point_1.confidence * point_2.confidence
+  // };
 
   double b_dot_d = ray_1.dot(ray_2);
   double a_dot_d = cam_1.dot(ray_2);
@@ -69,40 +154,19 @@ Point3d project_point(
     ((b_dot_d * b_dot_d) - 1)
   );
   double t = (
-    ((b_dot_d * (c_dot_d - (a_dot_d)) - (b_dot_c * a_dot_b))) /
+    ((b_dot_d * (c_dot_d - a_dot_d)) - (b_dot_c * a_dot_b)) /
     ((b_dot_d * b_dot_d) - 1)
   );
 
   cv::Mat midpoint = (cam_1 + cam_2 + (t * ray_1) + (s * ray_2)) / 2;
   return Point3d{
     .point_id = point_1.point_id,
-    .x = midpoint.at<double>(0, 0),
-    .y = midpoint.at<double>(1, 0),
-    .z = midpoint.at<double>(2, 0),
+    .x = midpoint.at<float>(0, 0),
+    .y = midpoint.at<float>(1, 0),
+    .z = midpoint.at<float>(2, 0),
     .confidence = point_1.confidence * point_2.confidence
   };
 }
-
-// std::vector<Point3d> project_camera_points(
-//   const CameraParameters& params,
-//   const std::vector<Point> points
-// ) {
-//   std::vector<Point3d> projected;
-//   std::vector<cv::Point3d> cv_projected;
-//   std::vector<cv::Point2d> cv_points;
-//   projected.reserve(points.size());
-//   cv_projected.reserve(points.size());
-//   cv_points.reserve(points.size());
-//   for (const Point& point : points) cv_points.push_back(point.coords);
-//   cv::projectPoints(
-//     cv_points,
-//     params.rotation,
-//     params.translation,
-//     params.matrix,
-//     params.distortion,
-//     cv_projected
-//   );
-// }
 
 std::vector<Point3d> project_points(
   const CameraParameters& params_1,
@@ -117,6 +181,16 @@ std::vector<Point3d> project_points(
     );
   }
   return points;
+}
+
+void save_obj(const cv::Mat& t, const Person3d& person, std::filesystem::path file) {
+  std::ofstream out{file};
+  // out << "v " << t.at<float>(0, 0) << ' ' << t.at<float>(1, 0) << ' ' << t.at<float>(2, 0) << '\n';
+  for (const Point3d& point : person.body) {
+    out << "v " << point.x << ' ' << point.y << ' ' << point.z << '\n';
+  }
+  out << std::flush;
+  out.close();
 }
 
 int main() {
@@ -167,6 +241,11 @@ int main() {
         )
       });
     }
-    save_people_3d(frame_3d, get_animation_directory_path() / entry.path().filename());
+    if (!frame_3d.empty()) {
+      save_people_3d(frame_3d, get_animation_directory_path() / entry.path().filename());
+      std::filesystem::path filename = entry.path().filename();
+      filename.replace_extension(".obj");
+      save_obj(cam_trans_to_world(cam_1_parameters), frame_3d.front(), get_animation_directory_path() / filename);
+    }
   }
 }
