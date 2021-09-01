b4d213d61d8fbceec8dd21c9c5660633849b9291
Natalie Wolfe <natalie@lifewanted.com>
2021-08-31 21:39:39 -0700

    Adjust camera recording.

--- recording/camera.py
+++ recording/camera.py
@@ -2,10 +2,19 @@ from picamera import PiCamera
 from time import sleep
 
 
-camera = PiCamera()
-camera.rotation = 180
+camera = PiCamera(
+  resolution=(1920, 1080),
+  framerate=30
+)
 camera.start_preview()
-camera.start_recording('/home/pi/recording/video.h264')
+camera.start_recording(
+  '/home/pi/video.h264',
+  level='4.2',
+  intra_period=60 * 60,
+  intra_refresh='cyclic',
+  bitrate = 50000000 # 50Mbps
+
+)
 sleep(60)
 camera.stop_recording()
 camera.stop_preview()
