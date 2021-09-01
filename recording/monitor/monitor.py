a5d7b13fb4f37d6cef12b0cb3c31e00ce2354ac7
Natalie Wolfe <natalie@lifewanted.com>
2021-08-31 21:34:57 -0700

    Create camera template page.

--- recording/monitor/monitor.py
+++ recording/monitor/monitor.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 app = Flask(__name__)
+env = Environment(loader=FileSystemLoader(""), autoescape=select_autoescape)
 
 @app.route("/")
 def index():
-  return 'yo'
+  template = env.get_template('recording/monitor/index.html')
+  return template.render()
 
 if __name__ == "__main__":
   app.run()
