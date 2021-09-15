554b441d8d8fa8df8cce7c6c8ffeebab4108b189
Natalie Wolfe <natalie@lifewanted.com>
2021-09-14 20:47:26 -0700

    More space for frames

--- recording/monitor/frame_saver.cpp
+++ recording/monitor/frame_saver.cpp
@@ -16,7 +16,7 @@ std::uint64_t FrameSaver::save_frame() {
 
 std::filesystem::path FrameSaver::frame_path(std::uint64_t frame_id) const {
   char frame_name[16] = {0};
-  std::snprintf(frame_name, sizeof(frame_name), "%06lu.jpg", frame_id);
+  std::snprintf(frame_name, sizeof(frame_name), "%07lu.jpg", frame_id);
   return _output_dir / frame_name;
 }
 
